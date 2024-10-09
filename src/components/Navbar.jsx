436ac005f1ce9cd762d9cdab02fc0b7d1f653786
nav2924 <navi29012004@gmail.com>
2024-10-09 14:14:32 +0530

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="py-5">
+        <div className="container mx-auto px-5 lg:px-20">
+          <div className="flex items-center justify-between">
+            <img
+              src="/logo.png"
+              height={40}
+              width={40}
+              className="rounded-full"
+            />
+            <img
+              src="/menu.svg"
+              alt="Menu Icon"
+              className="h-5 w-5 md:hidden"
+            />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About Us</a>
+              <a href="#">Internships</a>
+              <a href="#">Services</a>
+              <a href="#">Testimonials</a>
+              <a href="#">Find Us</a>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "../components/index";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
